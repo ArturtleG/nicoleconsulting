217230a53d2144cb54f06535b5f06b2c68100483
ArturtleG <28913861+ArturtleG@users.noreply.github.com>
2025-05-18 12:07:07 -0500

    Update version.js

--- js/version.js
+++ js/version.js
@@ -11,10 +11,27 @@ $(function(){
         success: function(data, status, xhr) {
           var lm = xhr.getResponseHeader('Last-Modified');
           if (lm) {
-            var v = new Date(lm).getTime();
-            $el.attr(attr, url + '?v=' + v);
+            var d    = new Date(lm);
+            var yyyy = d.getFullYear();
+            var MM   = String(d.getMonth() + 1).padStart(2, '0');
+            var dd   = String(d.getDate()).padStart(2, '0');
+            var hh   = String(d.getHours()).padStart(2, '0');
+            var mi   = String(d.getMinutes()).padStart(2, '0');
+            var ss   = String(d.getSeconds()).padStart(2, '0');
+            var msec = String(d.getMilliseconds()).padStart(3, '0');
+
+            var versionString = 
+              yyyy + '.' +
+              MM   + '.' +
+              dd   + '.' +
+              hh   + '.' +
+              mi   + '.' +
+              ss   + '.' +
+              msec;
+
+            $el.attr(attr, url + '?v=' + versionString);
           }
         }
       });
     });
-});
+  });
